6365368821aad7d681ff3151d5762559d000c3b2
iotplatform6 <50313065+iotplatform6@users.noreply.github.com>
2021-01-27 13:30:03 +0530

    added one test

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require("express");
 
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('hey world');
+app.get("/", function (req, res) {
+  res.send("hey world");
+});
+
+app.get("/data", function (req, res) {
+  res.send({ data: { msg: "yo" } });
 });
 
 app.listen(process.env.PORT || 3000);
--- test/test.js
+++ test/test.js
@@ -6,3 +6,9 @@ describe('GET /', function() {
     request(app).get('/').expect('hey world', done);
   });
 });
+
+describe('GET /data', function() {
+  it('respond with hello world', function(done) {
+    request(app).get('/').expect({data: {msg: "yo"}}, done);
+  });
+});
