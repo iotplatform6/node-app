ed38c43f445fe7f21e692f9caa1eaa95b0950155
iotplatform6 <50313065+iotplatform6@users.noreply.github.com>
2021-02-25 12:45:56 +0530

    Update test.js

--- test/test.js
+++ test/test.js
@@ -6,9 +6,3 @@ describe('GET /', function() {
     request(app).get('/').expect('hey world', done);
   });
 });
-
-describe('GET /data', function() {
-  it('respond with hello world', function(done) {
-    request(app).get('/').expect({data: {msg: "yo"}}, done);
-  });
-});
